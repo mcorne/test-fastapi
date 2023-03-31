5143c681acd32a877a06b82c0c83b86b42592cad
Michel Corne <mcorne@yahoo.com>
2023-03-31 16:08:10 +0200

    Add tags and exceptions

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from enum import Enum
-from typing import Annotated
+from typing import Annotated, Any
 
-from fastapi import Body, FastAPI, Path, Query
-from pydantic import BaseModel, Field, HttpUrl
+from fastapi import Body, FastAPI, HTTPException, Path, Query, Response, status
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel, EmailStr, Field, HttpUrl
+
+
+class Tags(Enum):
+    items = "items"
+    users = "users"
 
 
 class Image(BaseModel):
-    url: HttpUrl
+    url: str  # HttpUrl
     name: str
 
 
 class Item(BaseModel):
     name: str
     description: str | None = Field(default=None, title="The description of the item", max_length=300)
-    price: float = Field(gt=0, description="The price must be greater than zero")
+    price: float = Field(ge=0, description="The price must be greater than zero")
     tax: float | None = None
     images: list[Image] | None = None
+    tags: set[str] = set()
 
 
 class ModelName(Enum):
@@ -31,9 +38,14 @@ class Offer(BaseModel):
     items: list[Item]
 
 
-class User(BaseModel):
+class BaseUser(BaseModel):
     username: str
+    email: EmailStr
     full_name: str | None = None
+
+
+class UserIn(BaseUser):
+    password: str
 
 
 app = FastAPI()
@@ -41,11 +53,11 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.put("/items/{item_id}")
+@app.put("/items/{item_id}", tags=[Tags.items])
 async def change_item(
     item_id: int,
     item: Item | None = None,
-    user: User = None,
+    user: BaseUser = None,
     importance: Annotated[int, Body()] = None,
     q: str | None = None,
 ):
@@ -61,8 +73,8 @@ async def change_item(
     return results
 
 
-@app.post("/items/")
-async def create_item(item: Annotated[Item, Body(embed=True)]):
+@app.post("/items/", response_model=Item, tags=[Tags.items])
+async def create_item(item: Annotated[Item, Body(embed=True)]) -> dict:
     item_dict = item.dict()
     if item.tax:
         price_with_tax = item.price + item.tax
@@ -73,6 +85,11 @@ async def create_item(item: Annotated[Item, Body(embed=True)]):
 @app.post("/offers/")
 async def create_offer(offer: Offer):
     return offer
+
+
+@app.post("/user/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserIn) -> BaseUser:
+    return user
 
 
 @app.get("/models/{model_name}")
@@ -86,12 +103,25 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some residuals"}
 
 
+@app.get("/portal", response_model=None)
+async def get_portal(teleport: bool = False) -> Response | dict:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return {"message": "Here's your interdimensional portal."}
+
+
 @app.get("/items/{item_id}")
 async def read_item(
     item_id: Annotated[int, Path(title="The ID of the item to get", gt=1, le=1000)],
     q: Annotated[str | None, Query(alias="item-query")] = None,
     short: bool = False,
 ):
+    if item_id == 999:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Item 999 forbidden",
+            headers={"X-Error": "There goes my error"},
+        )
     item = {"item_id": item_id}
     if q:
         item.update({"q": q})
