70e80257907cb014e9d73bdebd0630b6fb142037
Michel Corne <mcorne@yahoo.com>
2023-04-05 12:36:27 +0200

    Add OAuth2

--- main.py
+++ main.py
@@ -3,13 +3,39 @@ from typing import Annotated, Any
 
 from fastapi import Body, Depends, FastAPI, HTTPException, Path, Query, Response, status
 from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel, EmailStr, Field, HttpUrl
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 
 class BaseUser(BaseModel):
     username: str
     email: EmailStr
     full_name: str | None = None
+
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
 
 
 class Image(BaseModel):
@@ -44,15 +70,22 @@ class Tags(Enum):
     users = "users"
 
 
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
 class UserIn(BaseUser):
     password: str
 
 
-class CommonQueryParams:
-    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
-        self.q = q
-        self.skip = skip
-        self.limit = limit
+class UserInDB(User):
+    hashed_password: str
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
@@ -62,9 +95,41 @@ async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 10
 CommonsDep = Annotated[dict, Depends(common_parameters)]
 
 
-app = FastAPI()
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+app = FastAPI()
 
 
 @app.put("/items/{item_id}", tags=[Tags.items])
@@ -104,6 +169,19 @@ async def create_offer(offer: Offer):
 @app.post("/user/", status_code=status.HTTP_201_CREATED, tags=[Tags.users])
 async def create_user(user: UserIn) -> BaseUser:
     return user
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
 
 
 @app.get("/models/{model_name}")
@@ -180,6 +258,11 @@ async def read_items(
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/users/me", tags=[Tags.users])
+async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
 
 
 @app.get("/")
